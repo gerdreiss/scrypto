de9b82eff4da29321016a2695d4c81db17ebc4ba
Gerd Reiss <gerdreiss@jscriptive.com>
2017-03-15 21:17:43 +0100

    First refactoring of the MainActivity - moved TextWatcher implementation to case class InputTextWatcher

--- src/main/scala/com/jscriptive/scrypto/MainActivity.scala
+++ src/main/scala/com/jscriptive/scrypto/MainActivity.scala
@@ -11,21 +11,13 @@ import android.widget.{EditText, Toast}
 
 class MainActivity extends AppCompatActivity {
   // allows accessing `.value` on TR.resource.constants
-  implicit val context = this
+  implicit val context: MainActivity = this
 
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     // type ascription is required due to SCL-10491
-    val vh: TypedViewHolder.main = TypedViewHolder.setContentView(this, TR.layout.main)
-    vh.input.addTextChangedListener(new TextWatcher {
-      override def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int): Unit = {}
-
-      override def afterTextChanged(s: Editable): Unit = {}
-
-      override def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int): Unit = {
-        vh.output.setText(Base64.encodeToString(s.toString.getBytes("UTF-8"), Base64.DEFAULT))
-      }
-    })
+    val vh = TypedViewHolder.setContentView(this, TR.layout.main).asInstanceOf[TypedViewHolder.main]
+    vh.input.addTextChangedListener(InputTextWatcher(vh.output))
     vh.image.getDrawable match {
       case a: Animatable => a.start()
       case _ =>
@@ -33,26 +25,34 @@ class MainActivity extends AppCompatActivity {
   }
 
   def send(view: View): Unit = {
-    val outputText: EditText = context.findViewById(R.id.output).asInstanceOf[EditText]
+    val outputText = findViewById(R.id.output).asInstanceOf[EditText]
     if (outputText.length() == 0) {
-      Toast.makeText(context, "Nothing to send", Toast.LENGTH_SHORT).show()
+      Toast.makeText(this, "Nothing to send", Toast.LENGTH_SHORT).show()
     } else {
-      Toast.makeText(context, "This button does nothing yet...", Toast.LENGTH_SHORT).show()
+      Toast.makeText(this, "This button does nothing yet...", Toast.LENGTH_SHORT).show()
     }
   }
 
   def copy(view: View): Unit = {
-    val outputText: EditText = context.findViewById(R.id.output).asInstanceOf[EditText]
+    val outputText = findViewById(R.id.output).asInstanceOf[EditText]
     if (outputText.length() == 0) {
-      Toast.makeText(context, "Nothing to copy", Toast.LENGTH_SHORT).show()
+      Toast.makeText(this, "Nothing to copy", Toast.LENGTH_SHORT).show()
     } else {
-      Option(context.getSystemService(Context.CLIPBOARD_SERVICE)) match {
+      Option(getSystemService(Context.CLIPBOARD_SERVICE)) match {
         case Some(manager: ClipboardManager) =>
           val clip = ClipData.newPlainText(outputText.getText.toString, outputText.getText.toString)
           manager.setPrimaryClip(clip)
-          Toast.makeText(context, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+          Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
         case _ =>
       }
     }
+  }
+}
+
+case class InputTextWatcher(output: EditText) extends TextWatcher {
+  override def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int): Unit = {}
+  override def afterTextChanged(s: Editable): Unit = {}
+  override def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int): Unit = {
+    output.setText(Base64.encodeToString(s.toString.getBytes("UTF-8"), Base64.DEFAULT))
   }
 }
