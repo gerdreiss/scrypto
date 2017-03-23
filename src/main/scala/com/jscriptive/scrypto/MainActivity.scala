58bea2de19be1b947407aa974a88cd2fe4ab2441
Gerd Reiss <gerdreiss@jscriptive.com>
2017-03-23 19:44:13 +0100

    Added encryption method selection

--- src/main/scala/com/jscriptive/scrypto/MainActivity.scala
+++ src/main/scala/com/jscriptive/scrypto/MainActivity.scala
@@ -2,7 +2,6 @@ package com.jscriptive.scrypto
 
 import android.content.ClipData.newPlainText
 import android.content.{ClipboardManager, Context}
-import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.text.{Editable, TextWatcher}
@@ -14,11 +13,32 @@ class MainActivity extends AppCompatActivity {
   // allows accessing `.value` on TR.resource.constants
   implicit val context: MainActivity = this
 
-  class InputTextWatcher(output: EditText) extends TextWatcher {
+  private def encodeBase64(input: String): String = {
+    Base64.encodeToString(input.getBytes("UTF-8"), Base64.DEFAULT)
+  }
+
+  private def encryptInput(input: String, encryption: String): String = {
+    val digest = java.security.MessageDigest.getInstance("MD5")
+    digest.reset()
+    digest.update(input.getBytes("UTF-8"))
+    new java.math.BigInteger(1, digest.digest()).toString(16)
+  }
+
+  private def updateOutput(input: CharSequence, encryptionMethodIndex: Int, output: EditText) = {
+    encryptionMethodIndex match {
+      case 0 => output.setText(encodeBase64(input.toString))
+      case 1 => output.setText(encryptInput(input.toString, "MD5"))
+      case 2 => output.setText(encryptInput(input.toString, "SHA-1"))
+      case 3 => output.setText(encryptInput(input.toString, "SHA-256"))
+      case _ =>
+    }
+  }
+
+  class InputTextWatcher(encryptionMethods: Spinner, output: EditText) extends TextWatcher {
     override def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int): Unit = {}
     override def afterTextChanged(s: Editable): Unit = {}
     override def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int): Unit = {
-      output.setText(Base64.encodeToString(s.toString.getBytes("UTF-8"), Base64.DEFAULT))
+      updateOutput(s, encryptionMethods.getSelectedItemPosition, output)
     }
   }
 
@@ -27,6 +47,16 @@ class MainActivity extends AppCompatActivity {
     def onItemSelected(parent: AdapterView[_], view: View, position: Int, id: Long): Unit = {
       if (position > 0) {
         input.setText(parent.getItemAtPosition(position).toString)
+        parent.setSelection(0)
+      }
+    }
+  }
+
+  class EncryptionMethodListener(input: EditText, output: EditText) extends AdapterView.OnItemSelectedListener {
+    def onNothingSelected(parent: AdapterView[_]): Unit = {}
+    def onItemSelected(parent: AdapterView[_], view: View, position: Int, id: Long): Unit = {
+      if (input.getText.length > 0) {
+        updateOutput(input.getText.toString, position, output)
       }
     }
   }
@@ -36,17 +66,14 @@ class MainActivity extends AppCompatActivity {
     // type ascription is required due to SCL-10491
     val vh = TypedViewHolder.setContentView(this, TR.layout.main).asInstanceOf[TypedViewHolder.main]
     vh.inputHistory.setOnItemSelectedListener(new InputHistoryListener(vh.input))
-    vh.input.addTextChangedListener(new InputTextWatcher(vh.output))
-    vh.image.getDrawable match {
-      case a: Animatable => a.start()
-      case _ =>
-    }
+    vh.input.addTextChangedListener(new InputTextWatcher(vh.encryptionMethods, vh.output))
+    vh.encryptionMethods.setOnItemSelectedListener(new EncryptionMethodListener(vh.input, vh.output))
   }
 
-  def send(view: View): Unit = {
+  def share(view: View): Unit = {
     val outputText = findViewById(R.id.output).asInstanceOf[EditText]
     if (outputText.length() == 0) {
-      Toast.makeText(this, "Nothing to send", Toast.LENGTH_SHORT).show()
+      Toast.makeText(this, "Nothing to share", Toast.LENGTH_SHORT).show()
     } else {
       Toast.makeText(this, "This button does nothing yet...", Toast.LENGTH_SHORT).show()
     }
