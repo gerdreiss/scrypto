70ebdba08bc7384003b8e70963b480206ddee0f1
Gerd Reiss <gerdreiss@jscriptive.com>
2017-03-25 19:57:54 +0100

    history

--- src/main/scala/com/jscriptive/scrypto/MainActivity.scala
+++ src/main/scala/com/jscriptive/scrypto/MainActivity.scala
@@ -1,5 +1,7 @@
 package com.jscriptive.scrypto
 
+import java.io._
+
 import android.content.ClipData.newPlainText
 import android.content.{ActivityNotFoundException, ClipboardManager, Context, Intent}
 import android.os.Bundle
@@ -11,7 +13,8 @@ import android.widget.{Toast, _}
 
 class MainActivity extends AppCompatActivity {
   // allows accessing `.value` on TR.resource.constants
-  implicit val context: MainActivity = this
+  implicit val context = this
+  val FILE = "scrypto.history"
 
   private def encodeBase64(input: String): String = {
     Base64.encodeToString(input.getBytes("UTF-8"), Base64.DEFAULT)
@@ -65,13 +68,14 @@ class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState)
     // type ascription is required due to SCL-10491
     val vh = TypedViewHolder.setContentView(this, TR.layout.main).asInstanceOf[TypedViewHolder.main]
+    //vh.inputHistory.setAdapter(new ArrayAdapter[String](context, vh.inputHistory.getId, readHistory(FILE).toArray))
     vh.inputHistory.setOnItemSelectedListener(new InputHistoryListener(vh.input))
     vh.input.addTextChangedListener(new InputTextWatcher(vh.encryptionMethods, vh.output))
     vh.encryptionMethods.setOnItemSelectedListener(new EncryptionMethodListener(vh.input, vh.output))
   }
 
   def share(view: View): Unit = {
-    val outputText = findViewById(R.id.output).asInstanceOf[EditText]
+    val outputText: EditText = findViewById(R.id.output).asInstanceOf[EditText]
     if (outputText.length() == 0) {
       Toast.makeText(this, "Nothing to share", Toast.LENGTH_SHORT).show()
     } else {
@@ -80,17 +84,18 @@ class MainActivity extends AppCompatActivity {
       sendIntent.putExtra(Intent.EXTRA_EMAIL, "recipient@example.com")
       sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Scrypto")
       sendIntent.putExtra(Intent.EXTRA_TEXT, outputText.getText.toString)
-      try
-        startActivity(Intent.createChooser(sendIntent, "Share..."))
-      catch {
-        case ex: ActivityNotFoundException =>
+      try {
+          startActivity(Intent.createChooser(sendIntent, "Share..."))
+          saveHistory(outputText.getText.toString)
+      } catch {
+        case _: ActivityNotFoundException =>
           Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT).show()
       }
     }
   }
 
   def copy(view: View): Unit = {
-    val outputText = findViewById(R.id.output).asInstanceOf[EditText]
+    val outputText: EditText = findViewById(R.id.output).asInstanceOf[EditText]
     if (outputText.length() == 0) {
       Toast.makeText(this, "Nothing to copy", Toast.LENGTH_SHORT).show()
     } else {
@@ -98,8 +103,38 @@ class MainActivity extends AppCompatActivity {
         case Some(manager: ClipboardManager) =>
           manager.setPrimaryClip(newPlainText(outputText.getText.toString, outputText.getText.toString))
           Toast.makeText(this, "Copied to clipboard", Toast.LENGTH_SHORT).show()
+          saveHistory(outputText.getText.toString)
         case _ =>
       }
     }
+  }
+
+  private def saveHistory(input: String): Unit = {
+    val lines: List[String] = readHistory(FILE)
+    val history = input :: lines.filterNot(_ == input)
+    saveNewHistory(FILE, history)
+  }
+
+  private def readHistory(filename: String): List[String] = {
+    val file = context.getFileStreamPath(filename)
+    if (file.exists()) {
+      val fileInput = openFileInput(filename)
+      val reader = new BufferedReader(new InputStreamReader(fileInput))
+      val lines = readLines(reader)
+      reader.close()
+      lines
+    } else Nil
+  }
+
+  private def readLines(reader: BufferedReader): List[String] = {
+    val line = reader.readLine()
+    if (line != null) line :: readLines(reader)
+    else Nil
+  }
+
+  private def saveNewHistory(filename: String, history: List[String]) = {
+    val fos = openFileOutput(filename, Context.MODE_PRIVATE)
+    fos.write(history.mkString("\n").getBytes("UTF-8"))
+    fos.close()
   }
 }
